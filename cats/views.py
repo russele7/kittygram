bb664d5cfe4c01fc360e9adb301bc757bb0f0c35
russele7 <russele7oge@gmail.com>
2024-02-24 17:14:05 +0400

    1

--- cats/views.py
+++ cats/views.py
@@ -1,11 +1,15 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
+# VIEW-ФУНКЦИИ
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
@@ -17,3 +21,82 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(pk=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
+# VIEW-КЛАССЫ
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+# VIEW-КЛАССЫ ДЖЕНЕРИКИ
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+# VIEW-SETS
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,36 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
-from cats.views import cat_list
+from cats.views import (
+    cat_list,
+    cat_detail,
+
+    APICat,
+    APICatDetail,
+
+    CatList,
+    CatDetail,
+
+    CatViewSet,
+)
+
+router = DefaultRouter() 
+router.register('cats', CatViewSet) 
 
 urlpatterns = [
-   path('cats/', cat_list),
-]
+   # VIEW-ФУНКЦИИ
+   # path('cats/', cat_list),
+   # path('cats/<int:pk>/', cat_detail),
 
+   # VIEW-КЛАССЫ
+   # path('cats/', APICat.as_view()),
+   # path('cats/<int:pk>/', APICatDetail.as_view())
+
+   # VIEW-КЛАССЫ ДЖЕНЕРИКИ
+   # path('cats/', CatList.as_view()),
+   # path('cats/<int:pk>/', CatDetail.as_view())
+   
+   # VIEWSETS
+   path('', include(router.urls)),
+]
 
